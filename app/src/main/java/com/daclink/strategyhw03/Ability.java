940205c3906bf3a8ba4e5ec262fd515c48fef2ed
Anne E. Hansen <hananniesen@gmail.com>
2024-04-20 18:59:09 -0700

    added JavaDoc comments

--- app/src/main/java/com/daclink/strategyhw03/Ability.java
+++ app/src/main/java/com/daclink/strategyhw03/Ability.java
@@ -1,5 +1,11 @@
 package com.daclink.strategyhw03;
-
+/**
+ * The `Ability` interface is a tagging interface
+ *
+ * @author  Anne Hansen
+ * @version 1.0
+ * @since   2024-04-20
+ */
 public interface Ability {
     // tagging interface (no methods)
 }
--- app/src/main/java/com/daclink/strategyhw03/MeleeAttack.java
+++ app/src/main/java/com/daclink/strategyhw03/MeleeAttack.java
@@ -1,11 +1,33 @@
 package com.daclink.strategyhw03;
 
+/**
+ * The `MeleeAttack` class implements the `Attack` interface, creates a `Monster`
+ * object called `attacker`, a public method `MeleeAttack()`, and overrides the `attack()`
+ * method from the `Attack` interface
+ *
+ * @author  Anne Hansen
+ * @version 1.0
+ * @since   2024-04-20
+ */
 public class MeleeAttack implements Attack {
     Monster attacker;
+    /**
+     * constructor `MeleeAttack()` that defines `attacker`
+     *
+     * @param attacker a Monster object that inflicts damage
+     * @since 2024-04-20
+     */
     public MeleeAttack(Monster attacker) {
         this.attacker = attacker;
     }
 
+    /**
+     * abstract method `attack()` that depletes the health of the `target`
+     *
+     * @param target a Monster object that takes damage
+     * @return Integer
+     * @since  2024-04-20
+     */
     @Override
     public Integer attack(Monster target) {
         String message = attacker + " uses a melee attack on " + target;
